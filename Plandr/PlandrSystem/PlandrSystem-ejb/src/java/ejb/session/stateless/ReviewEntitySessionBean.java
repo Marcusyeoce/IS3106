9eabb039612f7b0250eeeede0ddf39f5bb7f8fbc
Daniel Pham <e0313602@u.nus.edu>
2020-03-23 15:25:18 +0800

    Add ReviewEntitySessionBean

---
+++ Plandr/PlandrSystem/PlandrSystem-ejb/src/java/ejb/session/stateless/ReviewEntitySessionBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Pham The Dzung
+ */
+@Stateless
+public class ReviewEntitySessionBean implements ReviewEntitySessionBeanLocal {
+
+    // Add CRUD here
+}
