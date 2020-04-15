23cb89a07211d62cae1c6fb541d147e54d449999
Marcus <e0310209@u.nus.edu>
2020-04-16 00:51:12 +0800

    syncronized colour scheme for header and footer, added id to navigation of view article and attraction

--- Plandr/PlandrSystem/PlandrClient/src/app/view-article/view-article.component.ts
+++ Plandr/PlandrSystem/PlandrClient/src/app/view-article/view-article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-article',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewArticleComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+
+  constructor(private activatedRoute: ActivatedRoute) 
+  { 
+  }
 
   ngOnInit() {
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
 }
--- Plandr/PlandrSystem/PlandrClient/src/app/view-attraction/view-attraction.component.ts
+++ Plandr/PlandrSystem/PlandrClient/src/app/view-attraction/view-attraction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-attraction',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAttractionComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+
+  constructor(private activatedRoute: ActivatedRoute) 
+  { 
+  }
 
   ngOnInit() {
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
 }
