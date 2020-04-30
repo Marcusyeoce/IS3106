44db0cfcacdcc541e4aacd37d05a95651142c541
Daniel Pham <e0313602@u.nus.edu>
2020-04-30 18:55:00 +0800

    Added scaffolding

--- Plandr/PlandrSystem/PlandrClient/src/app/app.module.ts
+++ Plandr/PlandrSystem/PlandrClient/src/app/app.module.ts
@@ -1,28 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material";
 
-import { ButtonModule } from 'primeng/button';
+import { ButtonModule } from "primeng/button";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
-import { MainComponent } from './main/main.component';
-import { ViewAllArticlesComponent } from './view-all-articles/view-all-articles.component';
-import { ViewAllAttractionsComponent } from './view-all-attractions/view-all-attractions.component';
-import { ViewAllBookingsComponent } from './view-all-bookings/view-all-bookings.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { PaymentComponent } from './payment/payment.component';
-import { ProfileComponent } from './profile/profile.component';
-import { FaqComponent } from './faq/faq.component';
-import { ViewArticleComponent } from './view-article/view-article.component';
-import { ViewAttractionComponent } from './view-attraction/view-attraction.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MakeBookingComponent } from './make-booking/make-booking.component';
-import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { MainComponent } from "./main/main.component";
+import { ViewAllArticlesComponent } from "./view-all-articles/view-all-articles.component";
+import { ViewAllAttractionsComponent } from "./view-all-attractions/view-all-attractions.component";
+import { ViewAllBookingsComponent } from "./view-all-bookings/view-all-bookings.component";
+import { RegisterComponent } from "./register/register.component";
+import { LoginComponent } from "./login/login.component";
+import { PaymentComponent } from "./payment/payment.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { FaqComponent } from "./faq/faq.component";
+import { ViewArticleComponent } from "./view-article/view-article.component";
+import { ViewAttractionComponent } from "./view-attraction/view-attraction.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { MakeBookingComponent } from "./make-booking/make-booking.component";
+import { ProfileDetailsComponent } from "./profile-details/profile-details.component";
+import { ViewBookingComponent } from './view-booking/view-booking.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { WriteReviewComponent } from './write-review/write-review.component';
+import { SearchAttractionsComponent } from './search-attractions/search-attractions.component';
+import { CancelBookingComponent } from './cancel-booking/cancel-booking.component';
 
 @NgModule({
   declarations: [
@@ -41,16 +47,22 @@ import { ProfileDetailsComponent } from './profile-details/profile-details.compo
     HeaderComponent,
     FooterComponent,
     MakeBookingComponent,
-    ProfileDetailsComponent
+    ProfileDetailsComponent,
+    ViewBookingComponent,
+    ChangePasswordComponent,
+    SubscribeComponent,
+    WriteReviewComponent,
+    SearchAttractionsComponent,
+    CancelBookingComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ButtonModule,
-    MatButtonModule
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ Plandr/PlandrSystem/PlandrClient/src/app/search-attractions/search-attractions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-attractions',
+  templateUrl: './search-attractions.component.html',
+  styleUrls: ['./search-attractions.component.css']
+})
+export class SearchAttractionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
