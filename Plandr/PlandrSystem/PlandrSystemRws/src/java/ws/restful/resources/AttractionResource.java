790fa14469200fd32b10c47bcdc71630ea6709d1
Marcus <e0310209@u.nus.edu>
2020-05-01 18:18:35 +0800

    -done with attraction resource

--- Plandr/PlandrSystem/PlandrSystemRws/src/java/ws/restful/resources/AttractionResource.java
+++ Plandr/PlandrSystem/PlandrSystemRws/src/java/ws/restful/resources/AttractionResource.java
@@ -15,11 +15,15 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import util.exception.ArticleNotFoundException;
+import util.exception.AttractionNotFoundException;
 import ws.restful.model.ErrorRsp;
 import ws.restful.model.RetrieveAllAttractionsRsp;
+import ws.restful.model.RetrieveAttractionRsp;
 
 /**
  * REST Web Service
@@ -48,11 +52,14 @@ public class AttractionResource {
      * Retrieves representation of an instance of ws.restful.resources.AttractionResource
      * @return an instance of java.lang.String
      */
+    @Path("retrieveAllAttractions")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response retrieveAllAttractions() {
         
         try {
+            System.out.println("********** AttractionResource.retrieveAllAttractions()");
+            
             List<AttractionEntity> attractions = attractionEntitySessionBeanLocal.retrieveAllAttractions();
         
             RetrieveAllAttractionsRsp retrieveAllAttractionsRsp = new RetrieveAllAttractionsRsp(attractions);
@@ -64,6 +71,27 @@ public class AttractionResource {
             
             return Response.status(Status.INTERNAL_SERVER_ERROR).entity(errorRsp).build();
         }
-        
+    }
+    
+    @Path("retrieveAttraction/{attractionId}")
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveAttraction(@PathParam("attractionId") Long attractionId) {
+        try {
+            System.out.println("********** AttractionResource.retrieveAttraction()");
+            
+            AttractionEntity attraction = attractionEntitySessionBeanLocal.retrieveAttractionByAttractionId(attractionId);
+            
+            return Response.status(Status.OK).entity(new RetrieveAttractionRsp(attraction)).build();
+        } catch (AttractionNotFoundException ex) {
+            ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
+            
+            return Response.status(Status.BAD_REQUEST).entity(errorRsp).build();
+        } catch (Exception ex) {
+            ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
+            
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(errorRsp).build();
+        }
     }
 }
